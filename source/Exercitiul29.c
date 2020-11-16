f56688c07d6a32f7ff3c4522a9c8a8a3cec10c11
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 20:41:26 +0200

    solve ex 29

--- source/Exercitiul29.c
+++ source/Exercitiul29.c
@@ -4,36 +4,47 @@
 
 int checkPrime(int n);
 
-int main() {
-    int n, i, flag = 0;
-    printf("Introduceti un numar intreg pozitiv: ");
-    scanf("%d", &n);
+int main()
+{
+  int n, i, flag = 0;
+  printf("\nIntroduceti un numar intreg pozitiv: ");
+  scanf("%d", &n);
 
-    for (i = 2; i <= n / 2; ++i) {
-        if (checkPrime(i) == 1) {
-            if (checkPrime(n - i) == 1) {
-                printf("%d = %d + %d\n", n, i, n - i);
-                flag = 1;
-            }
-        }
+  for (i = 2; i <= n / 2; ++i)
+  {
+    if (checkPrime(i) == 1)
+    {
+      if (checkPrime(n - i) == 1)
+      {
+        printf("%d = %d + %d\n", n, i, n - i);
+        flag = 1;
+      }
     }
-    if (flag == 0)
-        printf("%d nu poate fi exprimat ca suma a doua numere prime]\n", n);
-    return 0;
+  }
+  if (flag == 0)
+  {
+    printf("%d nu poate fi exprimat ca suma a doua numere prime\n", n);
+  }
+  printf("\n");
+
+  return 0;
 }
 
 // function to check prime number
-int checkPrime(int n) {
-    int i, isPrime = 1;
-     //for-----
-     //if-----
-     //-----
-     //-----
-     //-----
-     //-----
-    return isPrime;
-}
+int checkPrime(int n)
+{
+  int i, isPrime = 1;
+  for (i = 2; i <= n / 2; ++i)
+  {
+    if (n % i == 0)
+    {
+      isPrime = 0;
+      break;
+    }
+  }
 
+  return isPrime;
+}
 
 /*
 -----output------
