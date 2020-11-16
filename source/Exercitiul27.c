45ec76a5974d5c851b9ed7805739377bc8548163
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 20:02:25 +0200

    solve ex 27

--- source/Exercitiul27.c
+++ source/Exercitiul27.c
@@ -2,24 +2,30 @@
 
 #include <stdio.h>
 
-int main() {
-    int n, reversedN = 0, rem, originalN;
-    printf("Introduceti un numar intreg cu mai multe cifre: ");
-    scanf("%d", &n);
-    originalN = n;
+int main()
+{
+  int n, reversedN = 0, rem, originalN;
+  printf("\nIntroduceti un numar intreg cu mai multe cifre: ");
+  scanf("%d", &n);
+  originalN = n;
 
-    while (n != 0) {
-     //-----
-     //-----
-     //-----
-    }
+  while (n != 0)
+  {
+    rem = n % 10;
+    reversedN = reversedN * 10 + rem;
+    n /= 10;
+  }
 
-    if (originalN == reversedN)
-        printf("%d este palindrom\n", originalN);
-    else
-        printf("%d nu este palindrom\n", originalN);
-
-    return 0;
+  if (originalN == reversedN)
+  {
+    printf("%d este palindrom\n", originalN);
+  }
+  else
+  {
+    printf("%d nu este palindrom\n", originalN);
+  }
+  printf("\n");
+  return 0;
 }
 
 /*
