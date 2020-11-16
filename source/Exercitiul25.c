025fdd4cc611dfb111dea546e1b5fa1df09a0c9b
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 19:41:37 +0200

    solve ex 25

--- source/Exercitiul25.c
+++ source/Exercitiul25.c
@@ -2,21 +2,27 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n1, n2, i, gcd, lcm;
-    printf("Introduceti doua numere intregi pozitive: ");
+    printf("\nIntroduceti doua numere intregi pozitive: ");
     scanf("%d %d", &n1, &n2);
 
-    for (i = 1; i <= n1 && i <= n2; ++i) {
-     //if-----
+    for (i = 1; i <= n1 && i <= n2; ++i)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+        {
             gcd = i;
-     //-----
+        }
     }
 
     lcm = (n1 * n2) / gcd;
 
-     //-----
-     //-----
+    printf("CMMMC al %d and %d is %d\n", n1, n2, lcm);
+    printf("CMMDC al %d and %d is %d\n", n1, n2, gcd);
+
+    printf("\n");
+
     return 0;
 }
 
