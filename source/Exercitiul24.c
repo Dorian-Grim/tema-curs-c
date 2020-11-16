8c759c49d44fc40df12053995ac6bf9a9586a4c7
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 18:43:32 +0200

    solve ex 24

--- source/Exercitiul24.c
+++ source/Exercitiul24.c
@@ -1,20 +1,24 @@
 /*cel mai mic multiplu comun */
 
 #include <stdio.h>
-int main() {
+int main()
+{
     int n1, n2, max;
-    printf("Introduceti doua numere intregi pozitive: ");
+    printf("\nIntroduceti doua numere intregi pozitive: ");
     scanf("%d %d", &n1, &n2);
 
-    max = (n1 > n2) ? n1 : n2; //--- (conditie ternara cu ? si :)
+    max = (n1 > n2) ? n1 : n2;
 
-    while (1) {
-        if (max % n1 == 0 && max % n2 == 0) {
+    while (1)
+    {
+        if (max % n1 == 0 && max % n2 == 0)
+        {
             printf("CMMMC al numerelor %d si %d este %d\n", n1, n2, max);
             break;
         }
         ++max;
     }
+    printf("\n");
     return 0;
 }
 
