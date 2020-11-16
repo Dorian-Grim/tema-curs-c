56d7c1ee1d64cddc3bb32d8a144daf82f2dd801e
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 04:28:12 +0200

    solve ex 15

--- source/Exercitiul15.c
+++ source/Exercitiul15.c
@@ -1,28 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int count, first_term = 0, second_term = 1, next_term, i;
+   int count, first_term = 0, second_term = 1, next_term, i;
 
-    printf("Introduceti numarul de termeni:\n");
-    scanf("%d",&count);
+   printf("Introduceti numarul de termeni:\n");
+   scanf("%d", &count);
 
-    printf("Primii %d termen al seriei Fibonacci:\n",count);
-    for ( i = 0 ; i < count ; i++ )
-    {
-       if ( i <= 1 )
-          next_term = i;
-       else
-       {
-     //-----
-     //-----
-     //-----
-       }
-       printf("Termenul %d: %d\n",i, next_term);
-    }
+   printf("Primii %d termen al seriei Fibonacci:\n", count);
+   for (i = 0; i < count; i++)
+   {
+      if (i <= 1)
+         next_term = i;
+      else
+      {
+         next_term = first_term + second_term;
+         first_term = second_term;
+         second_term = next_term;
+      }
+      printf("Termenul %d: %d\n", i, next_term);
+   }
 
-    return 0;
+   return 0;
 }
-
 
 /*
 -----output------
