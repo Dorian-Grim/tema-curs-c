f64e63f486b75613b28277fd5dddee464e226fcc
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 04:40:24 +0200

    solve ex 17

--- source/Exercitiul17.c
+++ source/Exercitiul17.c
@@ -1,22 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-   int num1,num2;
+  int num1, num2;
 
+  printf("\nIntroduceti doua numere diferite, separate de spatii:");
 
-   printf("\nIntroduceti doua numere diferite, separate de spatii:");
+  scanf("%i %i", &num1, &num2);
 
-     //scanf-----
+  if (num1 > num2)
+  {
+    printf("\n Numarul 1 mai mai mare decat numarul 2");
+  }
+  else if (num1 == num2)
+  {
+    printf("\n Cele doua numere sunt egale");
+  }
+  else
+  {
+    printf("\n Numarul 2 este mai mare decat numarul 1");
+  }
+  printf("\n\n");
 
-     //if-----
-      printf("\n Numarul 1 mai mai mare decat numarul 2");
-     //else if-----
-      printf("\n Cele doua numere sunt egale");
-     //else-----
-      printf("\n Numarul 2 este mai mare decat numarul 1");
-     //-----
-
-   return 0;
+  return 0;
 }
 
 /*
