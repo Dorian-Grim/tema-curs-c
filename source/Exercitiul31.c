1a4de8b20f52aea38346d6bd384a5c09e17ea5c6
Mihai Fologea <foxmihail83@gmail.com>
2020-11-16 21:01:04 +0200

    solve ex 31

--- source/Exercitiul31.c
+++ source/Exercitiul31.c
@@ -2,23 +2,28 @@
 
 #include <stdio.h>
 
-int main() {
-  int array[5];
+int main()
+{
+  int nums = 5;
+  int array[nums];
 
-  printf("Introduceti 5 numere intregi separate prin spatiu: ");
+  printf("\nIntroduceti %d numere intregi separate prin spatiu: ", nums);
 
-     //-----
-     //-----
-     //-----
+  for (int i = 0; i < nums; i++)
+  {
+    scanf("%d", &array[i]);
+  }
 
   printf("Ati introdus numerele: ");
 
-     //-----
-     //-----
-     //-----
+  for (int i = 0; i < nums; i++)
+  {
+    printf("%d\n", array[i]);
+  }
+
+  printf("\n");
   return 0;
 }
-
 
 /*
 -----output------
